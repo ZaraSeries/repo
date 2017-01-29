34b691b76f717d2b60b477643be2fcac8fc5191d
Zara Series <andreanimado@gmail.com>
2017-01-29 18:50:29 -0200

    Zara tv editado

--- plugin.video.Zara_TV/_Edit.py
+++ plugin.video.Zara_TV/_Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
-MainBase = 'http://pastebin.com/raw/UrLXXipC'
+MainBase = 'https://raw.githubusercontent.com/ZaraSeries/lista/master/ZaraTV.m3u'
 addon = xbmcaddon.Addon('plugin.video.Zara_TV')
