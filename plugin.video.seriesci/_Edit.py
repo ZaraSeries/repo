68f848d639f4883a5947b85aeaf2743efcce77bd
Zara Series <andreanimado@gmail.com>
2018-01-26 21:47:38 -0200

    Series si recolocados

--- plugin.video.seriesci/_Edit.py
+++ plugin.video.seriesci/_Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
-MainBase = 'https://raw.githubusercontent.com/ZaraSeries/lista/master/Series/Series%20Ci%20Principal.xml'
+MainBase = 'https://raw.githubusercontent.com/ZaraSeries/lista/master/App%20Descontinuado.xml'
 addon = xbmcaddon.Addon('plugin.video.seriesci')
